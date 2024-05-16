eb20166caac10ae543c2b952748e18a175bde652
halfmoon-mind <5338095@gmail.com>
2024-05-16 20:19:55 +0900

    feat: update

--- file-share/src/App.js
+++ file-share/src/App.js
@@ -3,37 +3,17 @@ import "./App.css";
 
 function App() {
     return (
-        <>
-            <head>
-                <title>P2P File Transfer</title>
-            </head>
-            <body>
-                <input type="file" id="fileInput" />
-                <button id="sendButton">Send File</button>
-                <br />
-                <div id="receivedFiles"></div>
-                <br />
-                <button id="downloadButton">Download Selected File</button>
-                <script src="webrtc.js"></script>
-            </body>
-        </>
-
-        // <div className="App">
-        //   <header className="App-header">
-        //     <img src={logo} className="App-logo" alt="logo" />
-        //     <p>
-        //       Edit <code>src/App.js</code> and save to reload.
-        //     </p>
-        //     <a
-        //       className="App-link"
-        //       href="https://reactjs.org"
-        //       target="_blank"
-        //       rel="noopener noreferrer"
-        //     >
-        //       Learn React
-        //     </a>
-        //   </header>
-        // </div>
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
     );
 }
 
