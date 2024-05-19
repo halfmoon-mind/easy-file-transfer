63438feab6a4805383f97b45274dc036639ac61c
halfmoon-mind <5338095@gmail.com>
2024-05-19 10:57:48 +0900

    fix: react error

--- file-share/src/App.js
+++ file-share/src/App.js
@@ -1,18 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <input type="file" id="fileInput" />
+            <button id="sendButton">Send File</button>
+            <br />
+            <div id="receivedFiles"></div>
+            <br />
+            <button id="downloadButton">Download Selected File</button>
         </div>
     );
 }
