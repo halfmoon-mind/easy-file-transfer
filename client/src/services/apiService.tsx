3503e853cfdb7df3b9d7bec835ed653dcdf568f0
halfmoon-mind <5338095@gmail.com>
2024-06-10 01:45:24 +0900

    feat: set http

--- client/src/services/apiService.tsx
+++ client/src/services/apiService.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "socket.easyfile.site:8080";
-const SOCKET_BASE_URL = "socket.easyfile.site:8081";
+const BASE_URL = "http://socket.easyfile.site:8080";
+const SOCKET_BASE_URL = "http://socket.easyfile.site:8081";
 
 const apiService = axios.create({
     baseURL: BASE_URL,
