51701d46df42f0cfbf2cb54e57ae873b80146490
halfmoon-mind <5338095@gmail.com>
2024-06-10 21:41:00 +0900

    feat: update not use 8080

--- client/src/services/apiService.tsx
+++ client/src/services/apiService.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "https://socket.easyfile.site:8080";
-const SOCKET_BASE_URL = "https://socket.easyfile.site:8081";
+const BASE_URL = "https://socket.easyfile.site";
+const SOCKET_BASE_URL = "https://socket.easyfile.site/socket";
 
 const apiService = axios.create({
     baseURL: BASE_URL,
