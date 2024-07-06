67d6840310d297b10f13aae424eb22b2260202b5
halfmoon-mind <5338095@gmail.com>
2024-07-06 17:31:41 +0900

    feat: Socket 통신 포맷 주석 업데이트

--- easyfile-client/src/types/SocketFormat.ts
+++ easyfile-client/src/types/SocketFormat.ts
@@ -1,3 +1,10 @@
+/**
+ * Socket 통신 포맷
+ * @interface SocketFormat
+ * @property {string} sender 송신자 Socket ID
+ * @property {string} receiver 수신자 Socket ID
+ * @property {any} data 메시지
+ */
 export default interface SocketFormat {
   /// 송신자 Socket ID
   sender: string;
